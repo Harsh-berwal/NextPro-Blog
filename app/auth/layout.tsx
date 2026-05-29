6851739ac52509cfab10693adf8da090a9495af3
Harsh <harshberwal240@gmail.com>
2026-05-29 11:29:12 +0530

    Created UI for SIgn Up page and installed convex

---
+++ app/auth/layout.tsx
@@ -0,0 +1,19 @@
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="absolute top-5 left-5">
+                <Link href="/" className={buttonVariants({ variant: "secondary" })}>
+                    <ArrowLeft className="size-4" />
+                    Go back
+                </Link>
+            </div>
+            <div className="w-full max-w-md mask-x-to-mauve-50 p-8 space-y-6 rounded-lg shadow">
+                {children}
+            </div>
+        </div>
+    );
+}
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,7 +1,103 @@
+"use client";
+
+import { signInSchema } from "@/app/schemas/auth";
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  Card,
+  CardTitle,
+} from "@/components/ui/card";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button, buttonVariants } from "@/components/ui/button";
+
 export default function SignUpPage() {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold">Sign Up Page</h1>
-        </div>
-    );
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit() { 
+    console.log(form.getValues());
+  };
+
+  return (
+    <Card >
+      <CardHeader>
+        <CardTitle>Sign Up</CardTitle>
+        <CardDescription>Create an account to get started.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup className="gap-y-4">
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field , fieldState }) => (
+                <Field>
+                  <FieldLabel>Full Name</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter your full name"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field , fieldState }) => (
+                <Field>
+                  <FieldLabel>Email</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter your email"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field , fieldState }) => (
+                <Field>
+                  <FieldLabel>Password</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="******"
+                    type="password"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Button type="submit" className={buttonVariants({ variant: "default" })}>
+                Sign Up
+            </Button>
+
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }
